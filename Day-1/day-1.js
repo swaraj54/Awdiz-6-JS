35d0d35ed49a02d9fee4446c52f5b39f861f7353
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-01-28 12:41:11 +0530

    Updates

---
+++ Day-1/day-1.js
@@ -0,0 +1,110 @@
+// datatypes : 
+
+// information 
+// name 
+// age 
+// number 
+// yes / no  boolean 
+
+// String - "", '', `` 
+// "true"
+//  'awdiz'
+//   `12345`
+
+// predefined function  - set of code 
+
+console.log("Awdiz")
+console.log(typeof("Pass@1234"))
+console.log('Awdiz')
+console.log(typeof('true'))
+console.log(`Awdiz`)
+console.log(typeof(`12.23`))
+
+console.log("ftyvbhj" , typeof(`iogyutfr`) , "87t6frtgvybu" ,"iuhygtfrd")
+console.log(2 + 2)
+
+Number :
+
+console.log(1234 ,  typeof(1234))
+console.log('1234', typeof('1234'))
+
+console.log(1 + 1)
+console.log("1" + '1')
+
+// Boolean 
+// true , false 
+
+console.log(true, typeof (true))
+console.log(false, typeof (false))
+console.log('true', typeof ('true'))
+
+// Varibles 
+//  data store 
+
+// var 
+// let 
+// const 
+
+var instituteName = "Awdiz"
+var age = 10
+var isOpen = true
+
+console.log(instituteName , age , isOpen)
+
+
+var value1 = 10;
+var value2 = 20;
+
+console.log(value1 + value2)
+console.log(value1 - value2)
+console.log(value1 * value2)
+console.log(value1 / value2)
+
+
+var kuchbhi = 12;
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+console.log(kuchbhi)
+
+
+var myValue = 10;
+console.log(myValue)
+myValue = 20; // re-assign 
+console.log(myValue)
+myValue = myValue + 10
+console.log(myValue)
+var myValue = 50; // re-declare
+console.log(myValue)
+
+
+const kuchbhi = 1234;
+kuchbhi = 29 
+const kuchbhi = 34567;
+console.log(kuchbhi)
+
+let kuchbhi = 1234;
+kuchbhi = 2345; // re-assign 
+let kuchbhi = 3455;
+console.log(kuchbhi)
+
+// var  - function scoped , hoisting is possible
+// re-assign and re-declare possible 
+// const  -  block scoped, hoisting is not possible
+// re-assign and re-declare not possible 
+// let  - block scoped, hoisting is not possible
+// re-assign is possible and re-declare is not possible
+
+// hoisting  js behavior 
+
+// myname = 'awdiz'; // assigning
+
+// console.log(myname) // log
+
+// var myname; // declare
+
+// myname = 'awdiz';
+
+// console.log(myname)
+
+// const , let myname ;
