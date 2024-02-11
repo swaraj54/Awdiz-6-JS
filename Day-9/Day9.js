4003151cb7203440c1be5177bde7efb5e60978de
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-02-11 13:13:02 +0530

    Updates

---
+++ Day-9/Day9.js
@@ -0,0 +1,118 @@
+// datatypes - String, number, Boolean 
+// varibles - var let const 
+// comparision operator - < > == === 
+// logical op || && 
+// conditional statement if else , switch 
+// Loops for - while 
+// ++ -- 
+// function  - trad, arraow
+// console.log();
+// typeof()
+// undefined 
+// Array  
+// function
+// Nested for loop 
+// Object
+// event loop 
+// setTimeout setInterval clearInterval
+// map foreach find findindex
+
+
+// setTimeout(() => {
+//     console.log("Excuted after 2000 msce.")
+//     console.log("Excuted after 2000 msce.")
+//     console.log("Excuted after 2000 msce.")
+// }, 5000);
+
+// // function MyFunction() {
+
+// // }
+// // MyFunction()
+
+// const MyFunction = (value1, value2) => {
+//     return value1 * value2
+// }
+
+// const myInterval = setInterval(() => { console.log("Excuting after every 2 sec..") }, 2000)
+
+// function StopInterval() {
+//     clearInterval(myInterval)
+// }
+
+// setTimeout(StopInterval, 5000)
+
+
+// console.log("1")
+// setTimeout(() => { console.log("2") }, 3000)
+// console.log("3")
+// setTimeout(() => { console.log("4") }, 1000)
+// console.log("5")
+// setTimeout(() => { console.log("6") }, 5000)
+// console.log("7")
+
+// //event loop
+// console.log(1)
+// setTimeout(()=>{console.log(2)}, 0)
+// console.log(3)
+// setTimeout(()=>{console.log(4)}, 2000)
+
+
+
+
+
+// // Predefined array function '
+
+// // Q Find number 5 indec from given array .
+
+// var array = [1, 2, 3, 4, 5, 6]
+
+// // for(var i = 0; i < array.length; i++){
+// //     if(array[i] == 5){
+// //         console.log(i)
+// //     }
+// // }\
+
+// const indec = array.findIndex((swaraj) => swaraj == 7)
+
+// console.log(indec)
+
+
+
+
+
+
+// var array = [1, 8, 2, 3, 4, 5, 6]
+
+// const answer = array.find((number) => number > 5)
+// console.log(answer)
+
+// // .map()
+// // .foreACH()
+// // .filter()
+// // .reduce()
+// // ,find()
+// // .findIndex()
+
+
+
+
+// var array = [1, 8, 2, 3, 4, 5, 6]
+// const result = array.map((num) => num > 5)
+// console.log(result)
+
+
+
+var array = [1, 6, 8, 2, 3, 4, 5, 6]
+array.forEach((number) => {
+    if (number > 5) {
+        console.log(number)
+    }
+})
+
+krushna
+pooja 
+athrva 
+sajid 
+akash 
+shahbaj 
+kurnaraj
