5dea658bd0f43e25715be75fbd660f0414ea309d
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-02-24 12:50:17 +0530

    Updaytes

---
+++ Day-15/Test.js
@@ -0,0 +1,9 @@
+var array = [{ name: "virat", runs: 1000 },
+    { name: "rohit", runs: 2000 },
+    { name: "rahul", runs: 3000 },]
+
+
+for (var i = 0; i < array.length; i++) {
+    console.log(array[i], i)
+    console.log(array[i].runs)
+}
